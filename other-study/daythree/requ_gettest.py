1422cd277fac22b4c1a6cec3e7f972cb5ffdff30
zgl19960907 <1334461271@qq.com>
2023-06-05 22:38:29 +0800

    jmeter相关

--- other-study/daythree/requ_gettest.py
+++ other-study/daythree/requ_gettest.py
@@ -1,3 +1,7 @@
+import base64
+import hashlib
+import time
+
 import requests
 
 '''
@@ -20,10 +24,21 @@ requests.post(url, json, headers)
     响应对象.status_code  响应状态码
     响应对象.json()  以json格式查看响应内容
 '''
-url = 'https://www.baidu.com'
-re = requests.get(url)
-print("请求地址", re.url)
-print("响应状态码", str(re.status_code))
-print("响应内容", re.text)
+# url = 'https://www.baidu.com'
+# re = requests.get(url)
+# print("请求地址", re.url)
+# print("响应状态码", str(re.status_code))
+# print("响应内容", re.text)
 
 # requests.post(url, data=)  #这里的参数一般使用data=，这样会防止敏感信息暴露
+# md5加密
+tokenStr = 'jiayouzglyouarebest'
+md5Str = hashlib.md5()
+t = str(round(time.time()*1000)) # 获取当前时间的毫秒数去掉小数点
+conn = t + tokenStr
+md5Str.update(conn.encode('utf-8'))
+print(md5Str.hexdigest())
+
+# base64加密
+base64Str = base64.b64encode(str(md5Str.hexdigest()).encode('utf-8')).upper()
+print(str(base64Str, encoding='utf-8'))
